b2f12481c1efafda448bd44a4370fa82be1968a5
TANIN <tanintjt@gmail.com>
2017-05-17 11:51:46 +0600

    fixed merged files

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,51 @@ use Illuminate\Http\Request;
 |
 */
 
+/*Route::get('abc', function () {
+    return 'Hello World';
+});*/
+
+
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
+
+
+
+Route::group(['prefix'=>'v1','namespace' => 'V1'], function () {
+
+    Route::get('get-login',['as' =>'get-login', 'uses'=>'UserLoginController@index']);
+
+});
+
+
+Route::group(['prefix'=>'v1','namespace' => 'V1','middleware' => 'auth.basic'], function () {
+
+
+    Route::get('abc', function (Request $request) {
+        //return $request->user();
+        return response()->json(['name'=>$request->user()->name,
+            'email'=>$request->user()->email,
+            'provider'=>$request->user()->provider]);
+
+    });
+
+    Route::post('post-login', [ 'as' => 'post-user-login', 'uses' => 'UserLoginController@login']);
+
+    //Route::match(array('GET', 'POST'), 'login', 'UserLoginController@login');
+
+    Route::post('user-logout',['as'=>'user-logout', 'uses'=>'UserLoginController@logout']);
+
+});
+
+
+
+
+
+
+
+
 
 
 
