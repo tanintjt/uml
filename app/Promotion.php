c0f9ad75d83ebff34c307e927e906bdb9c66025d
TANIN <tanintjt@gmail.com>
2017-05-23 12:37:06 +0600

    Models added according to db schema

---
+++ app/Promotion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Promotion extends Model
+{
+    protected $table = 'promotions';
+   
+   
+    protected $fillable = [
+        'name', 'information', 'status'
+    ];
+}
