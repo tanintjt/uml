dec028734325e60888706fe3a782f7834b78aebd
TANIN <tanintjt@gmail.com>
2017-06-18 15:38:59 +0600

    service center backend panel

--- app/Http/Controllers/Admin/ServiceCenterController.php
+++ app/Http/Controllers/Admin/ServiceCenterController.php
@@ -149,6 +149,7 @@ class ServiceCenterController extends Controller
      */
     public function update(Request $request, $id)
     {
+        
         $model = ServiceCenter::findOrFail($id);
 
         $rules = [
