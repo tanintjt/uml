e309f3236e6698c119b8ea6dd5cadff84b76f845
TANIN <tanintjt@gmail.com>
2017-07-25 09:46:40 +0600

    vehicle image show

--- resources/views/admin/vehicle/vehicle_image.blade.php
+++ resources/views/admin/vehicle/vehicle_image.blade.php
@@ -6,7 +6,7 @@
 
    <div class="modal-body">
 
-       <img src="{!! asset(isset($row->vehicle_image)?$row->vehicle_image:'') !!}" class="text-center">
+       <img src="{!! asset(isset($row->vehicle_image)?$row->vehicle_image:'') !!}"style="margin-left: 10px">
 
    </div>
    <div class="modal-footer">
