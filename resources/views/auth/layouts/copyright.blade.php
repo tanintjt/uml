6887f71b76b2bb68307541de2c9735ab3c5f73b1
TANIN <tanintjt@gmail.com>
2017-07-27 17:16:23 +0600

    login page

--- resources/views/auth/layouts/copyright.blade.php
+++ resources/views/auth/layouts/copyright.blade.php
@@ -1,3 +1,3 @@
 <footer class="auth-main-footer">
-    <p class="text-center">Powered By SHEBA TECHNOLOGIES LTD</p>
+    <p class="text-center text-bold">Powered By SHEBA TECHNOLOGIES LTD</p>
 </footer>
