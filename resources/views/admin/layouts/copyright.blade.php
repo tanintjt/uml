30b2743daeed31a6e6edc4ab1a5af48e227fd180
TANIN <tanintjt@gmail.com>
2017-07-27 17:46:40 +0600

    main footer

--- resources/views/admin/layouts/copyright.blade.php
+++ resources/views/admin/layouts/copyright.blade.php
@@ -1,3 +1,4 @@
+{{--
 <footer class="main-footer">
     <div class="pull-right">
         <b style="margin-left:80%">Powered By
@@ -5,5 +6,16 @@
         </b>
     </div>
     <p>&nbsp;</p>
+
+</footer>
+--}}
+
+
+<footer class="main-footer">
+    {{--<div class="pull-left hidden-xs">
+        --}}{{--<b>Version</b> 2.4.0--}}{{--
+    </div>--}}
+    <strong>Powered By
+        <a href="http://www.shebatech.com.bd/"><img src="{!! asset('public/themes/default/img/STL_Logo.png') !!}"style="width:8%;"></a></strong>
 
 </footer>
