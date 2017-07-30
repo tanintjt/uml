6aee1bc521cedc8c07171c1575893bbfa6275b1c
TANIN <tanintjt@gmail.com>
2017-07-30 09:59:00 +0600

    reset password email form

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,3 +1,4 @@
+{{--
 @extends('auth.layouts.master')
 
 @section('content')
@@ -41,4 +42,48 @@
         </div>
     </div>
 </div>
+@endsection
+--}}
+@extends('auth.layouts.master')
+
+@section('content')
+
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="{{ url('/') }}"><b>{!! config('app.name') !!}</b></a>
+        </div>
+
+        <div class="login-box-body">
+            <h3 class="login-box-msg">Reset Password</h3>
+            {!! Form::open(array('route' => 'password.email', 'name' => 'login-form', 'id' => 'login-form')) !!}
+
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="col-md-6 control-label">E-Mail Address</label>
+
+                <div class="col-xs-12">
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+
+            <div class="row">
+                <div class="col-xs-8 ">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat" style="background-color:#f05283;border-color:#f05283;margin-left:50% ">Send Password Reset Link</button>
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+
+        <div class="text-center">
+            <img src="{!! asset('public/themes/default/img/UML-Color-03-04.svg') !!}"style="width:52%">
+        </div>
+    </div>
+
 @endsection
