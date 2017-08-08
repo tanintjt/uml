e7934c28b8d4a4313d8728e979454ef897483afe
TANIN <tanintjt@gmail.com>
2017-08-08 17:37:58 +0600

    footer style changed

--- resources/views/auth/layouts/copyright.blade.php
+++ resources/views/auth/layouts/copyright.blade.php
@@ -2,7 +2,7 @@
     <p class="text-center">
         <b style="font-size: small">Powered By </b>
         <a href="http://www.shebatech.com.bd">
-           <img src="{!! asset('public/themes/default/img/STL_Logo.png') !!}"style="width:9%;">
+           <img src="{!! asset('public/themes/default/img/STL_Logo.png') !!}"style="width:12%;">
         </a>
     </p>
 
