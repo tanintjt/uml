0374cc507ca89c272c2cfd35a0bc6c0c0ca6f67c
TANIN <tanintjt@gmail.com>
2017-08-08 17:29:59 +0600

    footer style changed

--- resources/views/admin/layouts/copyright.blade.php
+++ resources/views/admin/layouts/copyright.blade.php
@@ -1,8 +1,6 @@
 <footer class="main-footer">
      <span style="margin-left:78%">
-        <strong>
-            Powered By
-        </strong>
+        <b style="font-size: small">Powered By </b>
         <a href="http://www.shebatech.com.bd">
             <img src="{!! asset('public/themes/default/img/STL_Logo.png') !!}"style="width:11%;">
         </a>
--- resources/views/auth/layouts/copyright.blade.php
+++ resources/views/auth/layouts/copyright.blade.php
@@ -1,3 +1,9 @@
 <footer class="auth-main-footer">
-    <p class="text-center text-bold">Powered By SHEBA TECHNOLOGIES LTD</p>
+    <p class="text-center">
+        <b style="font-size: small">Powered By </b>
+        <a href="http://www.shebatech.com.bd">
+           <img src="{!! asset('public/themes/default/img/STL_Logo.png') !!}"style="width:9%;">
+        </a>
+    </p>
+
 </footer>
