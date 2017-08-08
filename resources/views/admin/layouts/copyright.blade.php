334e30db91663ff12c72bb6dee74e5eb3b514b30
TANIN <tanintjt@gmail.com>
2017-08-08 16:22:07 +0600

    footer style changed

--- resources/views/admin/layouts/copyright.blade.php
+++ resources/views/admin/layouts/copyright.blade.php
@@ -1,7 +1,11 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b></b>
-    </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong>
+     <span style="margin-left: 80%">
+        <strong>
+            Powered By
+            <a href="http://www.shebatech.com.bd">
+                <img src="{!! asset('public/themes/default/img/STL_Logo.png') !!}"style="width:11%;">
+            </a>
+         </strong>
+     </span>
 </footer>
 
