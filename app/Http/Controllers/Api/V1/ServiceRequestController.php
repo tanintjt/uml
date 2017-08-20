b53adf2848c279b6d53a03176aebf5054d20360a
TANIN <tanintjt@gmail.com>
2017-08-20 13:47:51 +0600

    checked valid user to request a service

--- app/Http/Controllers/Api/V1/ServiceRequestController.php
+++ app/Http/Controllers/Api/V1/ServiceRequestController.php
@@ -3,61 +3,81 @@
 namespace App\Http\Controllers\api\V1;
 
 use App\ServiceRequest;
+use App\User;
+use App\UserVehicle;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 use Carbon\Carbon;
-
+use DB;
 class ServiceRequestController extends Controller
 {
 
 
     public function store(Request $request ){
 
-        $user = $request->user();
+        $user = UserVehicle::where('user_id',$request->user()->id)->first();
 
-        $rules = [
-            'service_center_id' => 'required',
-            'service_package_id' => 'required',
-        ];
+        if($user) {
 
-        $messages = [
-            'service_center_id.required' => 'Service Center is required!',
-            'service_package_id.required' => 'Service Package is required!',
-        ];
+            $purchase_date = Carbon::createFromFormat('Y-m-d H:s:i', $user->purchase_date);
+            $current_date = Carbon::createFromFormat('Y-m-d H:s:i', Carbon::now());
+            $interval = $purchase_date->diffInDays($current_date, false);
 
-        $validator = Validator::make($request->all(), $rules, $messages);
+            $service_count = ServiceRequest::where('user_id', $user->user_id)->where('status', 5)->count();
 
-        if ($validator->fails()) {
-            $result = $validator->errors()->all();
-            return response()->json($result, 400);
+            if ($service_count < 4 && $interval < 360) {
+
+                    $rules = [
+                        'service_center_id' => 'required',
+                        'service_package_id' => 'required',
+                    ];
+
+                    $messages = [
+                        'service_center_id.required' => 'Service Center is required!',
+                        'service_package_id.required' => 'Service Package is required!',
+                    ];
+
+                    $validator = Validator::make($request->all(), $rules, $messages);
+
+                    if ($validator->fails()) {
+                        $result = $validator->errors()->all();
+                        return response()->json($result, 400);
+                    }
+
+                    $date = Carbon::parse($request->input('request_date'));
+                    $service_request = ServiceRequest::create(
+                        [
+                            'user_id'   => $request->user()->id,
+                            'service_center_id'      => $request->input('service_center_id'),
+                            'service_package_id'     => $request->input('service_package_id'),
+                            'status'    => 1,
+                            'request_date' => $date->format('Y-m-d'),
+                            'request_time' => $date->format('H:i:s'),
+                            'special_request'=> $request->input('special_request')
+                        ]
+                    );
+
+                    if ($service_request) {
+                        $result = 'Successfully Sent Request';
+                        $http_code = 201;
+                    } else {
+                        $result = 'Request failed.';
+                        $http_code = 500;
+                    }
+                    return response()->json($result, $http_code);
+
+                }
+                else{
+                     return response()->json(
+                    'Date Expired !!', 401);
+                }
+        }else{
+            return response()->json(
+                'You are not entitled for free services', 401);
         }
 
-        $date = Carbon::parse($request->input('request_date'));
-        $service_request = ServiceRequest::create(
-            [
-                'user_id'   => $user->id,
-                'service_center_id'      => $request->input('service_center_id'),
-                'service_package_id'     => $request->input('service_package_id'),
-                'status'    => 1,
-                'request_date' => $date->format('Y-m-d'),
-                'request_time' => $date->format('H:i:s'),
-                'special_request'=> $request->input('special_request')
-            ]
-        );
-
-        if ($service_request) {
-            $result = 'Successfully Sent Request';
-            $http_code = 201;
-        } else {
-            $result = 'Request failed.';
-            $http_code = 500;
-        }
-
-        return response()->json($result, $http_code);
     }
-
-
 
 }
