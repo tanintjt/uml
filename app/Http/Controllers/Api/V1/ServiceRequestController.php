8a307581eab27a0e9771a156c1bda2d4d7e11f59
TANIN <tanintjt@gmail.com>
2017-08-22 13:04:13 +0600

    service request modified

--- app/Http/Controllers/Api/V1/ServiceRequestController.php
+++ app/Http/Controllers/Api/V1/ServiceRequestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\api\V1;
 
+use App\ServicePackage;
 use App\ServiceRequest;
 use App\User;
 use App\UserVehicle;
@@ -17,47 +18,54 @@ class ServiceRequestController extends Controller
 
     public function store(Request $request ){
 
-        $user = UserVehicle::where('user_id',$request->user()->id)->first();
+        $rules = [
+            'service_center_id' => 'required',
+            'service_package_id' => 'required',
+        ];
 
-        if($user) {
+        $messages = [
+            'service_center_id.required' => 'Service Center is required!',
+            'service_package_id.required' => 'Service Package is required!',
+        ];
 
-            $purchase_date = Carbon::createFromFormat('Y-m-d H:s:i', $user->purchase_date);
-            $current_date = Carbon::createFromFormat('Y-m-d H:s:i', Carbon::now());
-            $interval = $purchase_date->diffInDays($current_date, false);
+        $validator = Validator::make($request->all(), $rules, $messages);
 
-            $service_count = ServiceRequest::where('user_id', $user->user_id)->where('status', 5)->count();
+        if ($validator->fails()) {
+            $result = $validator->errors()->all();
+            return response()->json($result, 400);
+        }
 
-            if ($service_count < 4 && $interval < 360) {
+        $date = Carbon::parse($request->input('request_date'));
 
-                    $rules = [
-                        'service_center_id' => 'required',
-                        'service_package_id' => 'required',
-                    ];
+        $data =
+            [
+                'user_id'   => $request->user()->id,
+                'service_center_id'      => $request->input('service_center_id'),
+                'service_package_id'     => $request->input('service_package_id'),
+                'status'    => 1,
+                'request_date' => $date->format('Y-m-d'),
+                'request_time' => $date->format('H:i:s'),
+                'special_request'=> $request->input('special_request')
+            ];
 
-                    $messages = [
-                        'service_center_id.required' => 'Service Center is required!',
-                        'service_package_id.required' => 'Service Package is required!',
-                    ];
 
-                    $validator = Validator::make($request->all(), $rules, $messages);
+        $service_package = ServicePackage::where('id',$request->input('service_package_id'))->pluck('name');
 
-                    if ($validator->fails()) {
-                        $result = $validator->errors()->all();
-                        return response()->json($result, 400);
-                    }
+        if($service_package == "Free Service"){
 
-                    $date = Carbon::parse($request->input('request_date'));
-                    $service_request = ServiceRequest::create(
-                        [
-                            'user_id'   => $request->user()->id,
-                            'service_center_id'      => $request->input('service_center_id'),
-                            'service_package_id'     => $request->input('service_package_id'),
-                            'status'    => 1,
-                            'request_date' => $date->format('Y-m-d'),
-                            'request_time' => $date->format('H:i:s'),
-                            'special_request'=> $request->input('special_request')
-                        ]
-                    );
+            $user = UserVehicle::where('user_id',$request->user()->id)->first();
+
+            if($user) {
+
+                $purchase_date = Carbon::createFromFormat('Y-m-d H:s:i', $user->purchase_date);
+                $current_date = Carbon::createFromFormat('Y-m-d H:s:i', Carbon::now());
+                $interval = $purchase_date->diffInDays($current_date, false);
+
+                $service_count = ServiceRequest::where('user_id', $user->user_id)->where('status', 5)->count();
+
+                if ($service_count < 4 && $interval < 360) {
+
+                    $service_request =  ServiceRequest::create($data);
 
                     if ($service_request) {
                         $result = 'Successfully Sent Request';
@@ -70,14 +78,26 @@ class ServiceRequestController extends Controller
 
                 }
                 else{
-                     return response()->json(
-                    'Date Expired !!', 401);
+                    return response()->json(
+                        'Date Expired !!', 401);
                 }
-        }else{
-            return response()->json(
-                'You are not entitled for free services', 401);
-        }
+            }else{
+                return response()->json(
+                    'You are not entitled for free services.', 401);
+            }
 
+        }else{
+            $service_request =  ServiceRequest::create($data);
+
+            if ($service_request) {
+                $result = 'Successfully Sent Request';
+                $http_code = 201;
+            } else {
+                $result = 'Request failed.';
+                $http_code = 500;
+            }
+            return response()->json($result, $http_code);
+        }
     }
 
 }
