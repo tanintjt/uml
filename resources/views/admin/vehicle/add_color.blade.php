4d189864173a19254e833a324d53365e9a72135f
TANIN <tanintjt@gmail.com>
2017-09-28 15:49:30 +0600

    vehicle shows in asc order

--- resources/views/admin/vehicle/add_color.blade.php
+++ resources/views/admin/vehicle/add_color.blade.php
@@ -37,7 +37,7 @@
                     <div class="form-group{{ $errors->has('color_code') ? ' has-error' : '' }}">
                         {!! Form::label('color_code', 'Color:', ['class' => 'col-xs-2 control-label']) !!}
                         <div class="col-xs-9">
-                            {!! Form::text('color_code[]',  old('color_code'),['required','multiple' => true,'class' => 'code form-control', 'id' => 'color_code', 'placeholder' => 'color code']) !!}
+                            {!! Form::text('color_code[]',  old('color_code'),['required','multiple' => true,'class' => 'code form-control', 'id' => 'color_code', 'placeholder' => 'color code (ex: #2b2c31)']) !!}
                             @if ($errors->has('color_code'))
                                 <span class="help-block">
                                 <strong>{{ $errors->first('color_code') }}</strong>
@@ -77,7 +77,7 @@
                 '     <div class="form-group{{ $errors->has('color_code') ?' has-error' : '' }}">' +
                 '         {!! Form::label('color_code', 'Color:', ['class' => 'col-xs-2 control-label']) !!}' +
                 '             <div class="col-xs-9">' +
-                '                 {!! Form::text('color_code[]',  old('color_code'),['required','multiple' => true,'class' => 'code form-control','id' =>'color_code','placeholder' =>'color code']) !!}' +
+                '                 {!! Form::text('color_code[]',  old('color_code'),['required','multiple' => true,'class' => 'code form-control','id' =>'color_code','placeholder' =>'color code (ex: #2b2c31)']) !!}' +
                 '                       @if ($errors->has('color_code'))' +
                 '                          <span class="help-block">' +
                 '                              <strong>{{ $errors->first('color_code') }}</strong>' +
